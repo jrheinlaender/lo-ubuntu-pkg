5c791237d79a5d372ed57b4d1987abec0e1d844f
Chris Halls <halls@debian.org>
2005-11-04 10:52:08 +0000

    Imported openoffice_org-1_1beta-0pre1 Imported openoffice_org-1_1beta-0pre1 into debian-openoffice@lists.debian.org--oldcvs/openofficeorg--head--1.1.0

--- shell-lib.sh
+++ shell-lib.sh
@@ -6,13 +6,8 @@ trap "message;\
       exit 1" 1 2 3 15
 
 # call hook in openoffice.org-debian-files
-if [ -x /usr/share/openoffice.org-debian-files/install-hook ]; then
-  /usr/share/openoffice.org-debian-files/install-hook $THIS_SCRIPT $THIS_PACKAGE "$@"
-fi
-
-# old hook - DEPRECIATED
-if [ -x /usr/lib/openoffice.org-debian-files/install-hook ]; then
-  /usr/lib/openoffice.org-debian-files/install-hook $THIS_SCRIPT $THIS_PACKAGE "$@"
+if [ -x /usr/share/openoffice.org1.1-debian-files/install-hook ]; then
+  /usr/share/openoffice.org1.1-debian-files/install-hook $THIS_SCRIPT $THIS_PACKAGE "$@"
 fi
 
 #DEBHELPER#
