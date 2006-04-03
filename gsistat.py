2eb7242404f1c7288fffd9e450aba3c0bf7061b6
Matthias Klose <doko@ubuntu.com>
2006-04-03 12:26:39 +0000

    gsicheck install/rtf mime-type gsicheck install/rtf mime-type

--- gsistat.py
+++ gsistat.py
@@ -7,7 +7,7 @@ from translate.storage import oo
 def statistics(mf):
     helpfiles = [fn for fn in mf.listsubfiles() if fn.startswith('helpcontent2')]
     #print helpfiles
-    n_all = n_translated = 0
+    n_all = n_translated = n_untranslated = 0
     for helpfile in helpfiles:
         of = mf.getoofile(helpfile)
         #print helpfile, of.languages, len(of.ooelements)
@@ -15,6 +15,7 @@ def statistics(mf):
             n_all += 1
             if len(el.lines) == 1:
                 #print "WARNING: %s: no translation (%d lines)" % (helpfile, len(el.lines))
+                n_untranslated += 1
                 continue
             if len(el.lines) > 2:
                 print "WARNING: %s: too many translations (%d lines)" % (helpfile, len(el.lines))
@@ -27,7 +28,7 @@ def statistics(mf):
                 #print "                %s/%s/%s" % (el.lines[1].text, el.lines[1].helptext, el.lines[1].quickhelptext)
             else:
                 n_translated += 1
-    return n_all, n_translated
+    return n_all, n_translated, n_untranslated
 
 if __name__ == '__main__':
     for fn in sys.argv[1:]:
@@ -38,10 +39,10 @@ if __name__ == '__main__':
         except Exception, msg:
             sys.stdout.write("ERROR reading %s: %s\n" % (fn, msg))
 	    sys.stdout.flush()
-        all, translated = statistics(mf)
+        all, translated, untranslated = statistics(mf)
         try:
             ratio = translated / float(all) * 100
         except:
             ratio = 0.0
-        print "%15s: %4.1f%%, lines=%d, translated=%d" % (os.path.basename(fn), ratio, all, translated)
+        print "%15s: %4.1f%%, lines=%d, translated=%d, untranslated=%d" % (os.path.basename(fn), ratio, all, translated, untranslated)
 	sys.stdout.flush()
