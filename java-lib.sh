7cd08a6fd82fd824dd1dd86aba8274e5e822b177
Rene Engelhard <rene@debian.org>
2006-07-18 12:06:52 +0000

    remove obsolete java-lib.sh

--- java-lib.sh
+++
@@ -1,3 +0,0 @@
-java_diversions="usr/lib/openoffice/program/libj645${LIBSUFFIX}_g.so \
-                 usr/lib/openoffice/program/services.rdb \
-                 usr/lib/openoffice/share/registry/data/org/openoffice/Office/TypeDetection.xcu" 
