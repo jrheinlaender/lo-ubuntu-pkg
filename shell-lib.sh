ccc65ed4adc55e3e67b5f3a472327c98823d59a1
Rene Engelhard <rene@debian.org>
2009-10-04 02:40:54 +0200

    properly fix command-with-path-in-maintainer-script instead of overriding it

--- shell-lib.sh
+++ shell-lib.sh
@@ -59,16 +59,12 @@ rm_conffile() {
 check_for_running_ooo() {
 	if [ -e /usr/lib/openoffice/program/bootstraprc ]; then
 		LOCKFILE=`grep UserInstallation /usr/lib/openoffice/program/bootstraprc | cut -d= -f2 | sed -e 's,SYSUSERCONFIG,HOME,'`
-		if [ -x /usr/bin/pgrep ]; then
-		  PID=`/usr/bin/pgrep soffice.bin | head -n 1`
-		fi
+		PID=`pgrep soffice.bin | head -n 1`
 		if [ -n "$PID" ] || [ -e "$LOCKFILE" ]; then
 			db_input high openoffice.org/running
 			db_go
 			# try again in case OOo got closed before hitting OK
-			if [ -x /usr/bin/pgrep ]; then
-			  PID=`/usr/bin/pgrep soffice.bin | head -n 1`
- 			fi
+			PID=`pgrep soffice.bin | head -n 1`
 			if [ -n "$PID" ] || [ -e "$LOCKFILE" ]; then
 			  exit 1
 			fi
