2bc9f1871fac26b63cba2c5844d585ae449c06ff
Rene Engelhard <rene@debian.org>
2010-03-06 19:12:32 +0100

    debian/shell-lib-extensions.sh: handle non-existant basis-link

--- shell-lib-extensions.sh
+++ shell-lib-extensions.sh
@@ -53,10 +53,14 @@ remove_extension() {
   if /usr/lib/openoffice/program/unopkg list --shared $1 >/dev/null; then
     INSTDIR=`mktemp -d`
     export PYTHONPATH="/@OOBASISDIR@/program"
-    basis=`readlink /usr/lib/openoffice/basis-link`
+    if [ -L /usr/lib/openoffice/basis-link ]; then
+	d=/var/lib/openoffice/`readlink /usr/lib/openoffice/basis-link`/
+    else
+	d=/usr/lib/openoffice
+    fi
     /usr/lib/openoffice/program/unopkg remove -v --shared $1 \
       "-env:UserInstallation=file://$INSTDIR" \
-      "-env:UNO_JAVA_JFW_INSTALL_DATA=file:///var/lib/openoffice/$basis/share/config/javasettingsunopkginstall.xml" \
+      "-env:UNO_JAVA_JFW_INSTALL_DATA=file://$d/share/config/javasettingsunopkginstall.xml" \
       "-env:JFW_PLUGIN_DO_NOT_CHECK_ACCESSIBILITY=1"
     if [ -n $INSTDIR ]; then rm -rf $INSTDIR; fi
     flush_unopkg_cache
@@ -69,10 +73,14 @@ add_extension() {
   check_for_running_ooo
   INSTDIR=`mktemp -d`
   export PYTHONPATH="/@OOBASISDIR@/program"
-  basis=`readlink /usr/lib/openoffice/basis-link`
+  if [ -L /usr/lib/openoffice/basis-link ]; then
+      d=/var/lib/openoffice/`readlink /usr/lib/openoffice/basis-link`/
+  else
+      d=/usr/lib/openoffice
+  fi
   /usr/lib/openoffice/program/unopkg add -v --shared $1 \
     "-env:UserInstallation=file:///$INSTDIR" \
-    "-env:UNO_JAVA_JFW_INSTALL_DATA=file:///var/lib/openoffice/$basis/share/config/javasettingsunopkginstall.xml" \
+    "-env:UNO_JAVA_JFW_INSTALL_DATA=file://$d/share/config/javasettingsunopkginstall.xml" \
     "-env:JFW_PLUGIN_DO_NOT_CHECK_ACCESSIBILITY=1"
   if [ -n $INSTDIR ]; then rm -rf $INSTDIR; fi
   handle_soffice_listeners start
