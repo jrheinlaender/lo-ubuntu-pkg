b3ef5da06b3d25ca201c55b20ca0bd515a517904
Rene Engelhard <rene@debian.org>
2010-04-21 23:30:48 +0200

    cleanup. remove unused THIS_SCRIPT. Use dpkgs DPKG_MAINTSCRIPT_PACKAGE instead of THIS_SCRIPT

--- shell-lib-conffiles.sh
+++ shell-lib-conffiles.sh
@@ -6,7 +6,7 @@ prep_rm_conffile() {
         md5sum="`md5sum \"$CONFFILE\" | sed -e \"s/ .*//\"`"
         old_md5sum="`dpkg-query -W -f='${Conffiles}' $2 | grep $CONFFILE | awk '{print $2}'`"
         if [ "$md5sum" = "$old_md5sum" ]; then
-            mv "$CONFFILE" "$CONFFILE.${THIS_PACKAGE}-tmp"
+            mv "$CONFFILE" "$CONFFILE.${DPKG_MAINTSCRIPT_PACKAGE}-tmp"
         fi
     fi
 }
@@ -14,8 +14,8 @@ prep_rm_conffile() {
 rm_conffile_commit() {
   CONFFILE="$1"
 
-  if [ -e $CONFFILE.${THIS_PACKAGE}-tmp ]; then
-    rm $CONFFILE.${THIS_PACKAGE}-tmp
+  if [ -e $CONFFILE.${DPKG_MAINTSCRIPT_PACKAGE}-tmp ]; then
+    rm $CONFFILE.${DPKG_MAINTSCRIPT_PACKAGE}-tmp
   fi
 }
 
