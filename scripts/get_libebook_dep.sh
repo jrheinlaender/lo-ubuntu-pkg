ccaab15c95e23ac944454321827edcf444a7e800
Rene Engelhard <rene@debian.org>
2011-07-18 19:13:39 +0000

    Revert "fix get_libebook_dep script"

--- scripts/get_libebook_dep.sh
+++ scripts/get_libebook_dep.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-libs=`grep -o libebook.*\.so build/connectivity/source/drivers/evoab2/EApi.cxx|sort|uniq`
+libs=`grep libebook.*\.so build/connectivity/source/drivers/evoab2/EApi.cxx | perl -pe 's/\s+\"(.*)\".*/$1/'`
+
 for l in $libs; do
-	echo "Testing candidate library $l" >&2
 	if [ -e "/usr/lib/$l" ]; then
 		# sanity check: do the libs match with what we would get
 		# for our libebook version if we followed the .so symlink?
@@ -11,8 +11,6 @@ for l in $libs; do
 		l2=`readlink /usr/lib/$l2_tmp`
 		if [ "$l1" = "$l2" ]; then
 			dep=`dpkg -S /usr/lib/$l | cut -d: -f1`
-		else
-			echo "$l failed sanity check, because $l1 is not $l2." >&2
 		fi
 	fi
 done
@@ -20,8 +18,7 @@ done
 if [ -n "$dep" ]; then
 	echo $dep
 else
-	echo "Cannot find libebook dependency. None of the following libs found:" >&2
-	echo $libs >&2
+	echo "Cannot find libebook dependency. None of the following libs found:"	echo $libs
 	exit 1
 fi
 
