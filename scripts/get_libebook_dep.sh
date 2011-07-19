f4935132229d583866ddef15ebf8171097149957
Bjoern Michaelsen <bjoern.michaelsen@canonical.com>
2011-07-19 19:03:11 +0200

    remove superfluous multiarch hackery in get_libebook_dep

--- scripts/get_libebook_dep.sh
+++ scripts/get_libebook_dep.sh
@@ -3,16 +3,12 @@
 libs=`grep -o libebook.*\.so.[0-9]* build/connectivity/source/drivers/evoab2/EApi.cxx`
 for l in $libs; do
 	echo "Testing candidate library $l" >&2
-	libdir=/usr/lib
-	if [ `dpkg-architecture -qDEB_BUILD_ARCH` = "amd64" ]; then
-		libdir=/usr/lib`dpkg-architecture -qDEB_BUILD_ARCH_BITS`
-	fi
-	if [ -e "$libdir/$l" ]; then
+	if [ -e "/usr/lib/$l" ]; then
 		# sanity check: do the libs match with what we would get
 		# for our libebook version if we followed the .so symlink?
-		l1=`readlink $libdir/$l`
+		l1=`readlink /usr/lib/$l`
 		l2_tmp=`echo $l | grep -o libebook.*\.so`
-		l2=`readlink $libdir/$l2_tmp`
+		l2=`readlink /usr/lib/$l2_tmp`
 		if [ "$l1" = "$l2" ]; then
 			dep=`dpkg -S $l1 | cut -d: -f1`
 			# exit on success, we want the first (highest matching) API version
