e03d0bda634b567196c7297d344d400c70fdfebe
Bjoern Michaelsen <bjoern.michaelsen@canonical.com>
2011-09-09 18:52:59 +0200

    fix for binfilter in create-lo-update-patch.sh

--- scripts/create-lo-update-patch.sh
+++ scripts/create-lo-update-patch.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 REPOBASE=`readlink -f $1`
 FROMTAG=$2
@@ -28,21 +28,26 @@ for repo in \
     postprocess \
     sdk \
     testing \
-    translations \
     ure \
     writer
 do
-    if [ -d $REPOBASE/$repo.git ]
+    TAGPREFIX=
+    if [[ "$repo" == "binfilter" ]]
+    then
+        TAGPREFIX="${repo}_"
+    fi
+    if [[ -d $REPOBASE/$repo.git ]]
     then
         repodir=$REPOBASE/$repo.git
-    elif [ -d $REPOBASE/$repo ]
+    elif [[ -d $REPOBASE/$repo ]]
     then
         repodir=$REPOBASE/$repo
     else
         exit 1
     fi
     #echo "commits on repository $repo from $FROMTAG to $TOTAG"
-    git --git-dir=$repodir format-patch --stdout --subject-prefix="$repo" $FROMTAG..$TOTAG
+
+    git --git-dir=$repodir format-patch --stdout --subject-prefix="$repo" ${TAGPREFIX}${FROMTAG}..${TAGPREFIX}${TOTAG}
 done | sed \
     -e 's|^--- a/|--- |' \
     -e 's|^+++ b/|+++ |' \
