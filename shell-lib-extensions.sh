102e0480e6726fa33d5a72d2280e8a62ce2ff395
Rene Engelhard <rene@debian.org>
2012-04-12 13:27:24 +0200

    more upgrade fixes (make -core P-D: on ure, run unopkg list and run preinst of python-uno only on upgrades

--- shell-lib-extensions.sh
+++ shell-lib-extensions.sh
@@ -1,5 +1,7 @@
 validate_extensions() {
+  if /usr/lib/libreoffice/program/unopkg list --bundled >/dev/null 2>/dev/null; then
 	/usr/lib/libreoffice/program/unopkg validate -v --bundled
+  fi
 }
 
 sync_extensions() {
@@ -10,12 +12,14 @@ sync_extensions() {
   else
 	d=/usr/lib/libreoffice
   fi
-  if [ -e /usr/lib/libreoffice/share/prereg/bundled ] && readlink /usr/lib/libreoffice/share/prereg/bundled 2>&1 >/dev/null; then
-    /usr/lib/libreoffice/program/unopkg sync -v --bundled \
-      "-env:BUNDLED_EXTENSIONS_USER=file:///usr/lib/libreoffice/share/prereg/bundled" \
-      "-env:UserInstallation=file://$INSTDIR" \
-      "-env:UNO_JAVA_JFW_INSTALL_DATA=file://$d/share/config/javasettingsunopkginstall.xml" \
-      "-env:JFW_PLUGIN_DO_NOT_CHECK_ACCESSIBILITY=1"
+  if /usr/lib/libreoffice/program/unopkg list --bundled >/dev/null 2>/dev/null; then
+    if [ -e /usr/lib/libreoffice/share/prereg/bundled ] && readlink /usr/lib/libreoffice/share/prereg/bundled 2>&1 >/dev/null && [ -L $d/ure-link ]; then
+      /usr/lib/libreoffice/program/unopkg sync -v --bundled \
+        "-env:BUNDLED_EXTENSIONS_USER=file:///usr/lib/libreoffice/share/prereg/bundled" \
+        "-env:UserInstallation=file://$INSTDIR" \
+        "-env:UNO_JAVA_JFW_INSTALL_DATA=file://$d/share/config/javasettingsunopkginstall.xml" \
+        "-env:JFW_PLUGIN_DO_NOT_CHECK_ACCESSIBILITY=1"
+    fi
   fi
 }
 
