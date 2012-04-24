08bb5d4dd1028db97b6c7f008e8a9cdb11c7f3a3
Rene Engelhard <rene@debian.org>
2012-04-25 00:08:04 +0200

    also set HOME to $INSTDIR for unopkg (as -env=UserInstallation does not work (anymore)

--- shell-lib-extensions.sh
+++ shell-lib-extensions.sh
@@ -14,11 +14,13 @@ sync_extensions() {
   fi
   if /usr/lib/libreoffice/program/unopkg list --bundled >/dev/null 2>/dev/null; then
     if [ -e /usr/lib/libreoffice/share/prereg/bundled ] && readlink /usr/lib/libreoffice/share/prereg/bundled 2>&1 >/dev/null && [ -L $d/ure-link ]; then
-      /usr/lib/libreoffice/program/unopkg sync -v --bundled \
+      HOME=$INSTDIR \
+	/usr/lib/libreoffice/program/unopkg sync -v --bundled \
         "-env:BUNDLED_EXTENSIONS_USER=file:///usr/lib/libreoffice/share/prereg/bundled" \
         "-env:UserInstallation=file://$INSTDIR" \
         "-env:UNO_JAVA_JFW_INSTALL_DATA=file://$d/share/config/javasettingsunopkginstall.xml" \
         "-env:JFW_PLUGIN_DO_NOT_CHECK_ACCESSIBILITY=1"
+      rm -rf $INSTDIR
     fi
   fi
 }
