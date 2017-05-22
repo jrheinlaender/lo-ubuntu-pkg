37dc205eab8f9a707618073dd6faa2c739e2c02a
Rene Engelhard <rene@rene-engelhard.de>
2017-05-22 23:09:06 +0200

    remove obsolete create_package_directory gid_Module_Root_Files

--- scripts/gid2pkgdirs.sh
+++ scripts/gid2pkgdirs.sh
@@ -102,7 +102,6 @@ create_package_directory gid_Module_Optional_Pyuno_LibreLogo	pkg/libreoffice-lib
 create_package_directory gid_Module_Script_Provider_For_Python		pkg/libreoffice-script-provider-python
 create_package_directory gid_Module_Optional_Gnome              pkg/libreoffice-gnome
 create_package_directory gid_Module_Optional_Kde                pkg/libreoffice-kde
-create_package_directory gid_Module_Root_Files
 create_package_directory gid_Module_Optional_OGLTrans		pkg/libreoffice-ogltrans
 create_package_directory gid_Module_Root_SDK                    pkg/libreoffice-dev
 create_package_directory gid_Module_Optional_Extensions_Script_Provider_For_BS	pkg/libreoffice-script-provider-bsh
